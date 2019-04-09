4bce33f2f43f02e4636cef196aaa6a2d613bb23b
Antonio <toony1908@gmail.com>
2019-04-09 15:18:16 -0500

    add serch bar

--- views/Beers.js
+++ views/Beers.js
@@ -7,40 +7,58 @@
  */
 
 import React, {Component} from 'react';
-import {StyleSheet, ScrollView, Text} from 'react-native';
-import { ListItem } from 'react-native-elements'
+import {StyleSheet, ScrollView, Text, View} from 'react-native';
+import { ListItem, Input } from 'react-native-elements'
 const axios = require('axios');
 
 type Props = {};
 export default class Beers extends Component<Props> {
 
   state = {
-    beers: []
+    beers: [],
+    searchResults: []
   }
 
   componentDidMount() {
     axios.get('https://api.openbrewerydb.org/breweries').then(res => {
       this.setState({
-        beers: res.data
+        beers: res.data,
+        searchResults: res.data
       })
     }).catch(error => {
       console.log('el error:', error)
     })
   }
 
+  handleSearch = text => {
+    const results = this.state.beers.filter(element => {
+      return element.name.indexOf(text) > -1
+    })
+    this.setState({
+      ...this.state,
+      searchResults: results
+    })
+  }
+
   render() {
     return (
-      <ScrollView style={styles.container}>
-        <Text style={styles.text} > Holiiii </Text>
-        {this.state.beers.map((element, index) => (
-          <ListItem
-            key={index}
-            title={element.name}
-            subtitle={element.city}
-            onPress={() => this.props.navigation.navigate('Beer', element)}
-          />
-        ))}
-      </ScrollView>
+      <View>
+        <Input
+          placeholder='Search'
+          leftIcon={{ name: 'search' }}
+          onChangeText={this.handleSearch}
+        />
+        <ScrollView style={styles.container}>
+          {this.state.searchResults.map((element, index) => (
+            <ListItem
+              key={index}
+              title={element.name}
+              subtitle={element.city}
+              onPress={() => this.props.navigation.navigate('Beer', element)}
+            />
+          ))}
+        </ScrollView>
+      </View>
     );
   }
 }
